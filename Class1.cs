af91f7db0fe0aae6406b0d1eb47083654f87c0bd
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-02 09:37:57 +0200

    objektide orienteerumine

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class Mammal
+    {
+        public Mammal()
+        {
+            SilmadeArv = 2;
+            IsCarnivore = true;
+            Breathe();
+        }        
+        public int SilmadeArv { get; set; }
+        protected int Vanus { get; set; }
+        public bool IsCarnivore { get; set; }       
+        public void Breathe()
+        {
+            Console.WriteLine("I am breathin");
+        }        
+        public void SetAge(int age)
+        {
+            Vanus = age;
+        }
+        
+    }
+}
